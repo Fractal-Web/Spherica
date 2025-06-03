44a01d3e570268726368ccd8f5ebcc71eacfd6d8
cloud-ZER0 <29453komandor@gmail.com>
2025-06-03 15:18:15 +0400

    fix: build

--- src/app/api/twitter/route.ts
+++ src/app/api/twitter/route.ts
@@ -1,21 +1,25 @@
 // import { fetchTweets } from "@/app/integrations/twitter";
 // import { Post, TwitterAccount } from "@/app/integrations/twitter/types";
 
-// export async function POST(request: Request) {
-// 	const data = (await request.json()) as TwitterAccount[];
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function POST(request: Request) {
+	// const data = (await request.json()) as TwitterAccount[];
 
-// 	const posts: Post[] = [];
+	// const posts: Post[] = [];
 
-// 	const tweetsData = await Promise.all([data.map(acc => {
-// 		const tweets = fetchTweets({
-// 			account: acc,
-// 			token: `Bearer ${process.env.TWITTER_BEARER_TOKEN as string}`,
-// 		});
+	// const tweetsData = await Promise.all([
+	// 	data.map((acc) => {
+	// 		const tweets = fetchTweets({
+	// 			account: acc,
+	// 			token: `Bearer ${process.env.TWITTER_BEARER_TOKEN as string}`,
+	// 		});
+	// 	}),
+	// ]);
 
-// 	})])
+	// for (const account of data) {
+	// }
 
-// 	for(const account of data) {
-// 	}
+	// return Response.json({ tweets }, { status: 200 });
 
-// 	return Response.json({ tweets }, { status: 200 });
-// }
+	return Response.json("test");
+}
